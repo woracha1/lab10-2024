5bdbfea1f61ba76229197ab3334e9ea6492cc0e7
GGTTGTGt <zz814170@gmail.com>
2024-12-18 18:49:54 +0700

    lab10_3 completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,33 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    int count = 0 ;
+    float sum = 0 ;
+    float sumsqrt = 0 ;
+
+    string textline;
+    ifstream source("score.txt") ;
+    while(getline(source,textline)){
+        float score = stof(textline) ;
+        sum += score ;
+        sumsqrt += pow(score,2) ;
+        count++ ;
+    }
+    
+    float mean = sum/count ;
+
+    cout << "Number of data = " << count << "\n";
+
+    cout << setprecision(3);
+
+    cout << "Mean = " << sum/count << "\n";
+
+    cout << "Standard deviation = " << sqrt((sumsqrt/count)-pow(mean,2)) ;
+
+    return 0 ;
+}
